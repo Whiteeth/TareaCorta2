abe49fd5e21ce79b80a261b6ce5887b480c78812
jonathan <jonagaru1@hotmail.com>
2015-10-14 14:25:16 -0600

    s

--- StructBenchmark/src/cr/ac/tec/ce1103/structures/trees/SplayTrees.java
+++ StructBenchmark/src/cr/ac/tec/ce1103/structures/trees/SplayTrees.java
@@ -79,7 +79,7 @@ public class SplayTrees {
 	     }
 	 
 	     /** rotate **/
-	     public void makeRightChildParent(SplayNode c, SplayNode p)
+	     public void makeRightChildParent(NodeSplayTrees c, NodeSplayTrees p)
 	     {
 	         if ((c == null) || (p == null) || (p.right != c) || (c.parent != p))
 	             throw new RuntimeException("WRONG");
@@ -268,69 +268,5 @@ public class SplayTrees {
 
 
 	 
-	 /** Class SplayTreeTest **/
-	 public class SplayTreeTest
-	 {
-	    public static void main(String[] args)
-	    {            
-	        Scanner scan = new Scanner(System.in);
-	        /** Creating object of SplayTree **/
-	        SplayTrees spt = new SplayTrees(); 
-	        System.out.println("Splay Tree Test\n");          
-	        char ch;
-	        /**  Perform tree operations  **/
-	        do    
-	        {
-	            System.out.println("\nSplay Tree Operations\n");
-	            System.out.println("1. insert ");
-	            System.out.println("2. remove ");
-	            System.out.println("3. search");
-	            System.out.println("4. count nodes");
-	            System.out.println("5. check empty");
-	            System.out.println("6. clear tree");
-	 
-	            int choice = scan.nextInt();            
-	            switch (choice)
-	            {
-	            case 1 : 
-	                System.out.println("Enter integer element to insert");
-	                spt.insert( scan.nextInt() );                     
-	                break;
-	            case 2 : 
-	                System.out.println("Enter integer element to remove");
-	                spt.remove( scan.nextInt() );                     
-	                break;                          
-	            case 3 : 
-	                System.out.println("Enter integer element to search");
-	                System.out.println("Search result : "+ spt.search( scan.nextInt() ));
-	                break;                                          
-	            case 4 : 
-	                System.out.println("Nodes = "+ spt.countNodes());
-	                break;     
-	            case 5 : 
-	                System.out.println("Empty status = "+ spt.isEmpty());
-	                break;     
-	            case 6 : 
-	                System.out.println("\nTree Cleared");
-	                spt.clear();
-	                break;        
-	            default : 
-	                System.out.println("Wrong Entry \n ");
-	                break;   
-	            }
-	            /**  Display tree  **/
-	            System.out.print("\nPost order : ");
-	            spt.postorder();
-	            System.out.print("\nPre order : ");
-	            spt.preorder();
-	            System.out.print("\nIn order : ");
-	            spt.inorder(); 
-	 
-	            System.out.println("\nDo you want to continue (Type y or n) \n");
-	            ch = scan.next().charAt(0);                        
-	        } while (ch == 'Y'|| ch == 'y');               
-	    }
-	}
-	
 
-//drfg
+	
