ea4501c78eb350238179779715097901f5372508
Dilan Castro Enríquez <greekeshotmail.com>
2015-10-14 11:10:48 -0600

    Arreglos

---
+++ StructBenchmark/src/cr/ac/tec/ce1103/structures/benchmark/Arreglos.java
@@ -0,0 +1,64 @@
+package cr.ac.tec.ce1103.structures.benchmark;
+
+import java.util.Random;
+
+public class Arreglos {
+
+	public static void main(String[] args) {
+	Integer[] arreglo=crearInvertido(100);
+	Integer[] arreglo2=copiar(arreglo);
+	mostrarArreglo(arreglo2);
+		
+	}
+	
+	
+	//Método para crear arreglo random
+	public static Integer[] crearArregloRandom(int desde,int hasta,int tamaño){
+		Integer[] numeros = new Integer[tamaño];
+        Random rnd = new Random();
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = rnd.nextInt(hasta - desde + 1) + desde;
+        }
+        return numeros;
+
+	}
+	
+	//Método para imprimir arreglos
+	public static <T> void mostrarArreglo(T[] arreglo){
+		int k;
+		for(k=0;k<arreglo.length;k++){
+			System.out.print("["+arreglo[k]+"]");
+		}
+	}
+	
+	//Método para crear un arreglo entero ordenado de n elementos
+	public static Integer[] crearArreglo(int tamaño){
+		Integer[] arreglo= new Integer[tamaño];
+		for(int i=0; i<tamaño;i++){
+			arreglo[i]=i;
+		}
+		return arreglo;
+		
+	}
+	
+	//Método para crear un arreglo entero invertido de n elementos.
+	public static Integer[] crearInvertido(int tamaño){
+		Integer[] arreglo= new Integer[tamaño];
+		for(int i=0;i<arreglo.length;i++){
+			arreglo[i]=arreglo.length-i;
+		}
+		return arreglo;
+		
+	}
+	
+	//Método para copiar un arreglo
+	public static Integer[] copiar(Integer[] arreglo1){
+		Integer[] arreglo2 = new Integer[arreglo1.length];
+		for (int i = 0 ; i < arreglo1.length ; i++) {
+		    arreglo2[i] = arreglo1[i];
+		}
+		return arreglo2;
+	}
+	
+	
+}
