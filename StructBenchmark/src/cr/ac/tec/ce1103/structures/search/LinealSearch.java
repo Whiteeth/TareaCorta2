3c546d5cb007eba809f25e4129c7c27aacd1ef54
jonathan <jonagaru1@hotmail.com>
2015-10-14 08:22:33 -0600

    lineal search cambio

--- StructBenchmark/src/cr/ac/tec/ce1103/structures/search/LinealSearch.java
+++ StructBenchmark/src/cr/ac/tec/ce1103/structures/search/LinealSearch.java
@@ -9,4 +9,19 @@ package cr.ac.tec.ce1103.structures.search;
  */
 public class LinealSearch {
 
+	
+public boolean  search(int []array, int  element){
+	boolean r = false ;	
+	for(int i=0; i< array.length;  i++){
+			
+		if (array[i]==element){r=true;
+		}
+	
+	
+		
+		
+		}
+	return r;
+	}
+
 }
