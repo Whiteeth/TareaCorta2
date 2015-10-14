98e625166159844e08bbe23cad26b252e64c479f
dcastro96 <dcastro96@LAIMI1-A28.estudiantes.tec.cr>
2015-10-14 15:25:01 -0600

    Actualizacion de Bnchmark

--- StructBenchmark/src/cr/ac/tec/ce1103/structures/benchmark/Benchmark.java
+++ StructBenchmark/src/cr/ac/tec/ce1103/structures/benchmark/Benchmark.java
@@ -10,6 +10,8 @@ import cr.ac.tec.ce1103.structures.sort.SelectionSort;
 public class Benchmark {
 
 	public static void main(String[] args) {
+		
+		//Algoritmo para probar los timpos de los ordenamientos
 		Arreglos prueba =new Arreglos();
 		Integer[] arreglo= prueba.crearArreglo(10);
 		BubbleSort ordenar=new BubbleSort();
@@ -17,7 +19,65 @@ public class Benchmark {
 		System.out.println();
 		ordenar.burbuja(arreglo, 0, 0, null);
 		ordenar.mostrarArreglo(arreglo);
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		//Algoritmo para brobar los timpo de listas
+//		int cantidad=10000;
+//		ListaDoble lista_doble=new ListaDoble();
+//		lista_doble=hacerListaSimple(cantidad,lista_doble); //creacion de lista
+//		System.out.print(lista_doble.mostrarInicioFin());
+//		
+//		long tiempo=promedioBusqueda(lista_doble,1);
+//		System.out.println("Tiempo:\n"+tiempo);
 
 		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Método para crear una lista de n elmentos
+	public static ListaDoble hacerListaSimple(int cantidad,ListaDoble lista_doble){
+		for(int i=1;i!=cantidad+1;i++){
+			lista_doble.agregarInicio(i);
+		}
+		return lista_doble;
+	}
+	//Método que devuelve el tiempo en realizar una operacion
+	public static long promedioBusqueda(ListaDoble lista_doble,int elemento){
+		long time_start,time_end;
+		time_start=System.nanoTime();
+		lista_doble.borrarInicio();
+		time_end=System.nanoTime();
+		return time_end-time_start;
+	}
 		
 }
