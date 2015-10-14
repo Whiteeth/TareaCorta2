ae707302161d8ab0df28f6b1a1fbcd62910f7d21
dcastro96 <dcastro96@LAIMI1-A28.estudiantes.tec.cr>
2015-10-14 15:20:46 -0600

    Arreglo

--- StructBenchmark/src/cr/ac/tec/ce1103/structures/benchmark/Benchmark.java
+++ StructBenchmark/src/cr/ac/tec/ce1103/structures/benchmark/Benchmark.java
@@ -10,7 +10,7 @@ import cr.ac.tec.ce1103.structures.sort.SelectionSort;
 public class Benchmark {
 
 	public static void main(String[] args) {
-		PruebaOrdenamientos prueba =new PruebaOrdenamientos();
+		Arreglos prueba =new Arreglos();
 		Integer[] arreglo= prueba.crearArreglo(10);
 		BubbleSort ordenar=new BubbleSort();
 		ordenar.mostrarArreglo(arreglo);
