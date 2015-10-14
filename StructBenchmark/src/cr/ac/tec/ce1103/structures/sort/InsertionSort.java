25fe2f950133a8fb09965f092e0c8c77a90d95bf
Steven <maiderr97@gmail.com>
2015-10-14 00:17:06 -0600

    Actualizacion de Insertion Sort y Binary Tree

--- StructBenchmark/src/cr/ac/tec/ce1103/structures/sort/InsertionSort.java
+++ StructBenchmark/src/cr/ac/tec/ce1103/structures/sort/InsertionSort.java
@@ -1,5 +1,50 @@
 package cr.ac.tec.ce1103.structures.sort;
 
+import java.util.Random;
+
 public class InsertionSort {
+	public static class MyInsertionSort {
+		 
+	    public static void main(String[] args) {
+	    	long startTime = System.nanoTime();
+
+	    	int[]mejor = new int [1000];
+	    	int[]peor = new int [1000];
+	    	int[]promedio = new int [1000];
+	    	Random arreglo = new Random ();
+			 for (int i = 0; i < promedio.length; i++){
+				 int n  = arreglo.nextInt(1001);
+				 promedio[i]=n;
+			 }
+	        insertionSort(promedio);
+	        long endTime   = System.nanoTime();
+	        long totalTime = (endTime - startTime);
+	        System.out.println(totalTime + " nanosegundos");
+	        
+	    }
+	     
+	    public static void printNumbers(int[] input) {
+	         
+	        for (int i = 0; i < input.length; i++) {
+	            System.out.print(input[i] + ", ");
+	        }
+	        System.out.println("\n");
+	    }
+	 
+	    public static void insertionSort(int array[]) {
+	        int n = array.length;
+	        for (int j = 1; j < n; j++) {
+	            int key = array[j];
+	            int i = j-1;
+	            while ( (i > -1) && ( array [i] > key ) ) {
+	                array [i+1] = array [i];
+	                i--;
+	            }
+	            array[i+1] = key;
+	            printNumbers(array);
+	        }
+	    }
+	    
+	}    
 
 }
--- StructBenchmark/src/cr/ac/tec/ce1103/structures/trees/BinaryTree.java
+++ StructBenchmark/src/cr/ac/tec/ce1103/structures/trees/BinaryTree.java
@@ -1,5 +1,265 @@
 package cr.ac.tec.ce1103.structures.trees;
 
+import java.util.Random;
+
+import EstructurasJerarquicas.ArbolBinario.ArbolBinarioOrdenado;
+import EstructurasJerarquicas.ArbolBinario.ArbolBinarioOrdenado.Nodo;
+
 public class BinaryTree {
+	public static class ArbolBinarioOrdenado {
+	    class Nodo
+	      {
+	        int info;
+	        Nodo izq, der;
+	      }
+	      Nodo raiz;
+
+	      public ArbolBinarioOrdenado() {
+	          raiz=null;
+	      }
+	      
+	      public void insertar (int info)
+	      {
+	          Nodo nuevo;
+	          nuevo = new Nodo ();
+	          nuevo.info = info;
+	          nuevo.izq = null;
+	          nuevo.der = null;
+	          if (raiz == null)
+	              raiz = nuevo;
+	          else
+	          {
+	              Nodo anterior = null, reco;
+	              reco = raiz;
+	              while (reco != null)
+	              {
+	                  anterior = reco;
+	                  if (info < reco.info)
+	                      reco = reco.izq;
+	                  else
+	                      reco = reco.der;
+	              }
+	              if (info < anterior.info)
+	                  anterior.izq = nuevo;
+	              else
+	                  anterior.der = nuevo;
+	          }
+	      }
+
+
+	      private void imprimirPre (Nodo reco)
+	      {
+	          if (reco != null)
+	          {
+	              System.out.print(reco.info + " ");
+	              imprimirPre (reco.izq);
+	              imprimirPre (reco.der);
+	          }
+	      }
+
+	      public void imprimirPre ()
+	      {
+	          imprimirPre (raiz);
+	          System.out.println();
+	      }
+
+	      private void imprimirEntre (Nodo reco)
+	      {
+	          if (reco != null)
+	          {    
+	              imprimirEntre (reco.izq);
+	              System.out.print(reco.info + " ");
+	              imprimirEntre (reco.der);
+	          }
+	      }
+
+	      public void imprimirEntre ()
+	      {
+	          imprimirEntre (raiz);
+	          System.out.println();
+	      }
+
+
+	      private void imprimirPost (Nodo reco)
+	      {
+	          if (reco != null)
+	          {
+	              imprimirPost (reco.izq);
+	              imprimirPost (reco.der);
+	              System.out.print(reco.info + " ");
+	          }
+	      }
+
+
+	      public void imprimirPost ()
+	      {
+	          imprimirPost (raiz);
+	         
+	      }
+
+	      public static void main (String [] ar)
+	      {
+	          ArbolBinarioOrdenado abo = new ArbolBinarioOrdenado ();
+	          abo.insertar(10);
+	          for (int i = 0; i<=1000; i++){
+	        	  Random arreglo = new Random ();
+	        	  int n  = arreglo.nextInt(1001);
+	        	  abo.insertar(n);
+	        	  
+	          }
+//	          abo.insertar (40);
+//	          abo.insertar (20);
+//	          abo.insertar (60);
+//	          abo.insertar (10);
+//	          abo.insertar (30);
+//	          abo.insertar (50);
+//	          abo.insertar (70);
+//	          abo.insertar (45);
+//	          abo.insertar (55);
+//	          abo.insertar (54);
+	          long startTime = System.nanoTime();
+	          abo.insertar (10);
+	          long finalTime = System.nanoTime();
+			  long totalTime = finalTime - startTime;
+			  System.out.println(totalTime + " nanosegundos");
+	          System.out.println ("Impresion preorden: ");
+	          abo.imprimirPre ();
+	          //System.out.println ("Impresion entreorden: ");
+	          abo.borrar(10);
+	          //abo.borrarMayor();
+	          System.out.println ("Impresion preorden: ");
+	          abo.imprimirPre ();
+	          abo.buscar(10);
+	          //abo.imprimirEntre ();
+	          //System.out.println ("Impresion postorden: ");
+	          //abo.imprimirPost ();        
+	      }      
+	      public void borrarMenor() {
+	          if (raiz!=null) {
+	              if (raiz.izq==null)
+	                  raiz=raiz.der;
+	              else {
+	                  Nodo atras=raiz;
+	                  Nodo reco=raiz.izq;
+	                  while (reco.izq!=null) {
+	                      atras=reco;
+	                      reco=reco.izq;
+	                  }
+	                  atras.izq=reco.der;
+	              }                  
+	          }
+	      }
+	      public void borrarMayor() {
+	          if (raiz!=null) {
+	              if (raiz.der==null)
+	                  raiz=raiz.der;
+	              else {
+	                  Nodo atras=raiz;
+	                  Nodo reco=raiz.der;
+	                  while (reco.der!=null) {
+	                      atras=reco;
+	                      reco=reco.der;
+	                  }
+	                  atras.der=reco.der;
+	              }                  
+	          }
+	      }
+	      public boolean borrar(int valor) {
+	    	  Nodo auxiliar = raiz;
+	    	  Nodo padre = raiz;
+	    	  boolean esHijoIzq = true;
+	    	  while(auxiliar.info!=valor){
+	    		  padre=auxiliar;
+	    		  if (valor<auxiliar.info){
+	    			  esHijoIzq=true;
+	    			  auxiliar=auxiliar.izq;
+	    		  }else{
+	    			  esHijoIzq=false;
+	    			  auxiliar=auxiliar.der;
+	    		  }
+	    		  if (auxiliar==null){
+	    			  return false;
+	    		  }  
+	    	  }//Fin del while
+	    	  if (auxiliar.izq==null && auxiliar.der==null){
+	    		  if (auxiliar==raiz){
+	    			  raiz=null;
+	    			  
+	    		  }else if(esHijoIzq){
+	    			  padre.izq=null;
+	    			  
+	    		  }else{
+	    			  padre.der=null;
+	    		  }
+	    	  }else if(auxiliar.der==null){
+	    		  if (auxiliar==raiz){
+	    			  raiz=auxiliar.izq;
+	    			  
+	    		  }else if(esHijoIzq){
+	    			  padre.izq=auxiliar.izq;
+	    			  
+	    		  }else{
+	    			  padre.der=auxiliar.izq;
+	    		  }
+	    	  }else if(auxiliar.izq==null){
+	    		  if (auxiliar==raiz){
+	    			  raiz=auxiliar.der;
+	    			  
+	    		  }else if(esHijoIzq){
+	    			  padre.izq=auxiliar.der;
+	    			  
+	    		  }else{
+	    			  padre.der=auxiliar.izq;
+	    		  }
+	    	  }else{
+	    		  Nodo reemplazo=obtenerNodoReemplazo(auxiliar);
+	    		  if (auxiliar==raiz){
+	    			  raiz=reemplazo;
+	    		  }else if(esHijoIzq){
+	    			  padre.izq=reemplazo;
+	    		  }else{
+	    			  padre.der=reemplazo;
+	    		  }
+	    		  reemplazo.izq=auxiliar.izq;
+	    	  }
+	    	  return true;
+	    }
+	    //Metodo encargado de delvolvernos el nodo Reemplazo
+	    public Nodo obtenerNodoReemplazo (Nodo nodoReemp){
+	    	Nodo reemplazarPadre=nodoReemp;
+	    	Nodo reemplazo=nodoReemp;
+	    	Nodo auxiliar=nodoReemp.der;
+	    	while(auxiliar!=null){
+	    		reemplazarPadre=reemplazo;
+	    		reemplazo=auxiliar;
+	    		auxiliar=auxiliar.izq;
+	    	}
+	    	if (reemplazo!=nodoReemp.der){
+	    		reemplazarPadre.izq=reemplazo.der;
+	    		reemplazo.der=nodoReemp.der;
+	    	}
+	    	return reemplazo;
+	    			
+	    }
+	  public void buscar(int valor){
+		  Nodo temp = raiz;
+		  if (valor==raiz.info){
+			  System.out.println("El nodo es la Raiz: " + raiz.info);
+		  }else{
+			  while (temp!=null){
+				  if (temp.info==valor){
+					  System.out.println("El nodo es: " + temp.info);
+					  break;
+				  }else if(temp.info>valor){
+					  temp=temp.izq;
+				  }else{
+					  temp=temp.der;
+				  }
+			  }
+		  }
+	  }
+	      
+	      
+	}
 
 }
